f1d3c0352e6f2ee0d5e1316bf082752bb3f2527a
Jeff-Fang723 <846347771@qq.com>
2021-10-18 11:12:24 +0800

    Syntax error fixed

---
+++ addressbook/main.c
@@ -0,0 +1,22 @@
+#include "contacts.h"
+#include "operations.h"
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    
+    loop();
+    // contacts contact;
+    // contact = create_contact("Tingyu", "15906669899", "Zhejiang");
+    // int i = 0;
+    // while(contact.telnum[i] != 0)
+    // {
+    //     printf("%c", contact.telnum[i]);
+    //     i++;
+    // }
+    
+    // printf("%s,%s,%s\n", contact.name, contact.telnum, contact.adress);
+    // int a = 2, b = 3;
+    // Stu test = getStu(a, b);
+    // printf("%d, %d\n", test.a, test.b);
+    return 0;
+}
